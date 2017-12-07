d6884c4e9189234f49ee936a0d2ce464e3ffe08a
Cole K <colekurashige@gmail.com>
2017-12-07 01:07:41 -0800

    Comment 4.py

--- 4.py
+++ 4.py
@@ -1,20 +1,28 @@
 import sys
 from collections import Counter
 
-def soln(pwd, hashfn):
-    words = set()
-    for word in pwd.split(' '):
-        hashable = hashfn(word)
-        if hashable in words:
-            return False
-        words.add(hashable)
+def valid_password(pwd, hashfn):
+    '''
+        Takes a password pwd and "hash" function hashfn that returns a hashable
+        value given a string.
+    '''
+    hashed_words = set()
+    for hashed_word in pwd.split(' '):
+        hashable = hashfn(hashed_word)
+        # If the password has duplicated hashed words, it's invalid.
+        if hashable in words: return False
+        hashed_words.add(hashable)
+    # If the password has no duplicated hashed words, it's valid.
     return True
 
-result_1, result_2 = 0, 0
+result_1 = result_2 = 0
 identity = lambda x : x
+# "Hash" using Counters, which means anagrams hash to the same value.
 counter_hash = lambda x: frozenset(Counter(x).items())
 for line in sys.stdin: 
-    result_1 += soln(line.strip(), identity)
-    result_2 += soln(line.strip(), counter_hash)
+    # Passwords need to have no duplicated words.
+    result_1 += valid_password(line.strip(), identity)
+    # Passwords need to have no duplicated anagrams.
+    result_2 += valid_password(line.strip(), counter_hash)
 
 print('Part 1: {}, Part 2: {}'.format(result_1, result_2))
