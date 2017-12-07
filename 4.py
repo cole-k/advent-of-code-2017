6370623e1276de439be350aed4d20a677c65638b
Cole K <colekurashige@gmail.com>
2017-12-07 01:10:21 -0800

    Make sure 4.py works

--- 4.py
+++ 4.py
@@ -10,19 +10,19 @@ def valid_password(pwd, hashfn):
     for hashed_word in pwd.split(' '):
         hashable = hashfn(hashed_word)
         # If the password has duplicated hashed words, it's invalid.
-        if hashable in words: return False
+        if hashable in hashed_words: return False
         hashed_words.add(hashable)
     # If the password has no duplicated hashed words, it's valid.
     return True
 
-result_1 = result_2 = 0
+part_1 = part_2 = 0
 identity = lambda x : x
 # "Hash" using Counters, which means anagrams hash to the same value.
 counter_hash = lambda x: frozenset(Counter(x).items())
 for line in sys.stdin: 
     # Passwords need to have no duplicated words.
-    result_1 += valid_password(line.strip(), identity)
+    part_1 += valid_password(line.strip(), identity)
     # Passwords need to have no duplicated anagrams.
-    result_2 += valid_password(line.strip(), counter_hash)
+    part_2 += valid_password(line.strip(), counter_hash)
 
-print('Part 1: {}, Part 2: {}'.format(result_1, result_2))
+print('Part 1: {}, Part 2: {}'.format(part_1, part_2))
